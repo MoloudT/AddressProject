8470221432610f8583337836860b0808dedc3e3f
Mld Thryn <mld.thryn@outlook.com>
2023-03-07 01:52:25 +0100

    1-Add NuGet package: EntityFrameworkCore 2-create a database context

---
+++ AddressProject/DB/DataContex.cs
@@ -0,0 +1,18 @@
+﻿using AddressProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressProject.DB
+{
+    public class DataContex:DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DataContex(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+       
+        public DbSet<Address> Address { get; set; }
+
+    }
+}
+
--- AddressProject/Models/Address.cs
+++ AddressProject/Models/Address.cs
@@ -2,5 +2,11 @@
 {
     public class Address
     {
+        public int Id { get; set; }
+        public string? Street { get; set; }
+        public string? HouseNumber { get; set; }
+        public string? ZipCode { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
     }
 }
